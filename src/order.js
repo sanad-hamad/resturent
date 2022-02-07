f4bcb7611caee405d9bc38c87958f0c36b83c1a3
Sanad Hamad <45130829+SanadHamadGit@users.noreply.github.com>
2022-02-07 14:52:17 +0200

    update jquery to ready

--- src/order.js
+++ src/order.js
@@ -1,49 +1,42 @@
-import receiptDes from "./receipt.js"
+import receiptDes from "./receipt.js";
 
+let firstName, meale, price, datanow, ordername;
 
-let firstName, meale, price,datanow, ordername;
+function getDataFromUrl() {
+  new URLSearchParams(window.location.search).forEach((v, k) => {
+    // to get the data from request of signup
+    if (k === "firstName") {
+      console.log("the forst name is added ");
+      sessionStorage.setItem(k, v);
+      console.log(sessionStorage.getItem("firstName"));
+    }
 
-function getDataFromUrl()
-{
-    
-   
-    
-    new URLSearchParams(window.location.search).forEach((v, k) => {
-          // to get the data from request of signup  
-        if (k === "firstName") {
-            console.log("the forst name is added ")
-            sessionStorage.setItem(k, v);
-            console.log(sessionStorage.getItem("firstName"))
-    
-        }
-         
-        // ternary opreator to save data after ordring page submit 
-        k === "ordername" ? ordername = v : k === "meale" ? meale = v : k === "";
-    
-    
-    })
-   
-     let obj = JSON.parse(meale);
-     price=obj.price
-     console.log(obj);
-    
+    // ternary opreator to save data after ordring page submit
+    k === "ordername"
+      ? (ordername = v)
+      : k === "meale"
+      ? (meale = v)
+      : k === "";
+  });
+
+  let obj = JSON.parse(meale);
+  price = obj.price;
+  console.log(obj);
 }
 
-
-
-
-$(function(){
-    // get data from url after submit
-    getDataFromUrl();
-    // hide the order page 
-    $("#order").css("display","none");
-    //display the recipt page 
-    $("#recipt").css("display","block")
-    // call function to retrive the data 
-    receiptDes(sessionStorage.getItem("firstName"),ordername,price,new Date(),true);
-       
-
-})
-
-
-
+$(document).ready(function () {
+  // get data from url after submit
+  getDataFromUrl();
+  // hide the order page
+  $("#order").css("display", "none");
+  //display the recipt page
+  $("#recipt").css("display", "block");
+  // call function to retrive the data
+  receiptDes(
+    sessionStorage.getItem("firstName"),
+    ordername,
+    price,
+    new Date(),
+    true
+  );
+});
